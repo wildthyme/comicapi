c57bda958e56cf1ae23bec1cde974b4c424b2eb4
Iris Wildthyme <46726098+wildthyme@users.noreply.github.com>
2019-04-02 20:51:29 -0400

    bumped pypdf2 version requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
         description = 'Comic archive (cbr/cbz) and metadata utilities. Extracted from the comictagger project.',
         author = 'Iris W',
         packages = ['comicapi'],
-        install_requires = ['natsort==3.5.2', 'pypdf2==1.24', 'unrar==0.3'],
+        install_requires = ['natsort==3.5.2', 'pypdf2==1.26', 'unrar==0.3'],
         python_requires = '>=3.6.0',
         url = 'https://github.com/wildthyme/comicapi',
         classifiers = ['License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)']
