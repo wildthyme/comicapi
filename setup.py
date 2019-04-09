cb279168f9c5cec742b5a05ac8326b9c168a8a91
Iris W <46726098+wildthyme@users.noreply.github.com>
2019-04-09 15:18:28 -0400

    fixed syntax error

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         install_requires = ['natsort==3.5.2', 'pypdf2==1.26'],
         extras_require = {
             'CBR': ['unrar==0.3']
-        }
+        },
         python_requires = '>=3.6.0',
         url = 'https://github.com/wildthyme/comicapi',
         classifiers = ['License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)']
